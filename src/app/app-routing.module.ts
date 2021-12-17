3d2687d1981471878a2e080dc131b366f7497688
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-17 07:42:03 -0800

    Eager loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component'
+import { managerModuleRoutes } from './manager/manager.module'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'manager', children: managerModuleRoutes },
   { path: '**', component: PageNotFoundComponent },
 ]
 
