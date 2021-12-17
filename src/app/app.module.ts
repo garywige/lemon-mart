910f018f48e05c125cbe1276eb8d2b44e110ed43
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-16 19:45:03 -0800

    Generate Modules & Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { InventoryModule } from './inventory/inventory.module'
+import { ManagerModule } from './manager/manager.module'
 import { MaterialModule } from './material.module'
+import { PosModule } from './pos/pos.module'
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
