478c8be4b22a1b90f9dcfb8e81bada343daca3e1
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-16 20:37:40 -0800

    Custom Icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core'
+import { MatIconRegistry } from '@angular/material/icon'
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
   template: `
     <mat-toolbar color="primary">
+      <mat-icon svgIcon="lemon"></mat-icon>
       <button mat-button routerLink="/home">{{ title }}</button>
     </mat-toolbar>
     <router-outlet></router-outlet>
@@ -12,4 +15,11 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'LemonMart'
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'lemon',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/lemon.svg')
+    )
+  }
 }
