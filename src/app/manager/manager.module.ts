b78d76f8c7a0f897952f8955bd25cada8c8b07b2
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-17 13:51:05 -0800

    Style Fix

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -5,8 +5,8 @@ import { MaterialModule } from '../material.module'
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
 import { ManagerRoutingModule } from './manager-routing.module'
 import { ManagerComponent } from './manager.component'
-import { UserManagementComponent } from './user-management/user-management.component'
 import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component'
+import { UserManagementComponent } from './user-management/user-management.component'
 
 @NgModule({
   declarations: [
