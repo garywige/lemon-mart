9cb3d05a70f2be44f82518a2fb795038b76f0699
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-18 08:31:18 -0800

    AuthService

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@angular/core'
+import jwtDecode from 'jwt-decode'
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  filter,
+  flatMap,
+  map,
+  tap,
+  throwError,
+} from 'rxjs'
+
+import { transformError } from '../common/common'
+import { IUser, User } from '../user/user/user'
+import { Role } from './auth.enum'
+
+@Injectable()
+export abstract class AuthService implements IAuthService {
+  readonly authStatus$: BehaviorSubject<IAuthStatus>
+  readonly currentUser$: BehaviorSubject<IUser>
+
+  constructor() {
+    this.authStatus$ = new BehaviorSubject<IAuthStatus>(defaultAuthStatus)
+    this.currentUser$ = new BehaviorSubject<IUser>(new User())
+  }
+
+  login(email: string, password: string): Observable<void> {
+    const loginResponse$ = this.authProvider(email, password).pipe(
+      map((value) => {
+        const token = jwtDecode(value.accessToken)
+        return this.transformJwtToken(token)
+      }),
+      tap((status) => this.authStatus$.next(status)),
+      filter((status: IAuthStatus) => status.isAuthenticated),
+      flatMap(() => this.getCurrentUser()),
+      map((user) => this.currentUser$.next(user)),
+      catchError(transformError)
+    )
+
+    loginResponse$.subscribe({
+      error: (err) => {
+        this.logout()
+        return throwError(err)
+      },
+    })
+
+    return loginResponse$
+  }
+
+  logout(clearToken?: boolean): void {
+    setTimeout(() => this.authStatus$.next(defaultAuthStatus), 0)
+  }
+
+  getToken(): string {
+    throw new Error('Method not implemented.')
+  }
+
+  protected abstract authProvider(
+    email: string,
+    password: string
+  ): Observable<IServerAuthResponse>
+  protected abstract transformJwtToken(token: unknown): IAuthStatus
+  protected abstract getCurrentUser(): Observable<User>
+}
+
+export interface IAuthStatus {
+  isAuthenticated: boolean
+  userRole: Role
+  userId: string
+}
+
+export interface IServerAuthResponse {
+  accessToken: string
+}
+
+export const defaultAuthStatus: IAuthStatus = {
+  isAuthenticated: false,
+  userRole: Role.None,
+  userId: '',
+}
+
+export interface IAuthService {
+  readonly authStatus$: BehaviorSubject<IAuthStatus>
+  readonly currentUser$: BehaviorSubject<IUser>
+  login(email: string, password: string): Observable<void>
+  logout(clearToken?: boolean): void
+  getToken(): string
+}
