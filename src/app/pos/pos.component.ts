424c3eb618b044858dd121c623978e43f6dab605
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-17 17:35:02 -0800

    POS Component

---
+++ src/app/pos/pos.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pos',
+  template: `
+    <p>
+      pos works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
