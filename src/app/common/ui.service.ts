ae6fb9d91dc18d67ce8a3fe6e8c9e118031b88f5
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-19 14:19:03 -0800

    UI Service w/ showToast method

---
+++ src/app/common/ui.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core'
+import { MatDialog } from '@angular/material/dialog'
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UiService {
+  constructor(private snackBar: MatSnackBar, private dialog: MatDialog) {}
+
+  showToast(message: string, action = 'Close', config?: MatSnackBarConfig) {
+    this.snackBar.open(message, action, config || { duration: 7000 })
+  }
+}
