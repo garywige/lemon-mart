8abe122c10429525d6501ff882191f1eef6d9b77
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-20 20:41:04 -0800

    Fixed duplicate declaration
    of NavigationMenuComponent in the user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
-import { NavigationMenuComponent } from '../navigation-menu.component'
 import { LogoutComponent } from './logout/logout.component'
 import { ProfileComponent } from './profile/profile.component'
 import { UserRoutingModule } from './user-routing.module'
 
 @NgModule({
-  declarations: [ProfileComponent, LogoutComponent, NavigationMenuComponent],
+  declarations: [ProfileComponent, LogoutComponent],
   imports: [CommonModule, UserRoutingModule],
 })
 export class UserModule {}
